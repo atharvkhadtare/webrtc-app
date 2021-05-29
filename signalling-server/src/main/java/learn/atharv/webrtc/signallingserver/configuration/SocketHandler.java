3c8668ee1237b0f2243164ed3eb70996195881c3
atharvkhadtare <atharvkhadtare@gmail.com>
2021-05-29 11:10:37 +0530

    Complete steps 1 to 5 of baeldung tutorial: https://www.baeldung.com/webrtc

---
+++ signalling-server/src/main/java/learn/atharv/webrtc/signallingserver/configuration/SocketHandler.java
@@ -0,0 +1,33 @@
+package learn.atharv.webrtc.signallingserver.configuration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@Component
+@Slf4j
+public class SocketHandler extends TextWebSocketHandler {
+
+    List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
+
+    @Override
+    public void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
+        log.info("handleTextMessage: {}, {}", session.getId(), message);
+        for (WebSocketSession webSocketSession : sessions) {
+            if (webSocketSession.isOpen() && !session.getId().equals(webSocketSession.getId())) {
+                webSocketSession.sendMessage(message);
+            }
+        }
+    }
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
+    }
+}
